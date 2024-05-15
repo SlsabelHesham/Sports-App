0793fb806377b405621f42e4fc53cabc97790c70
Slsabel Hesham <slabelhesham@MacBook-Air.local>
2024-05-16 01:43:14 +0300

    setup storyboard

---
+++ SportsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportsApp
+//
+//  Created by Slsabel Hesham on 15/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
