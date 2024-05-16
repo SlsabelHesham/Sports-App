fd5329ad1487483347444dfa263684c3e982f4ce
mohamedelghaish <kotbm055@gmail.com>
2024-05-16 22:27:58 +0300

    AddAllSportsUI

---
+++ SportsApp/HomeScreenCell.swift
@@ -0,0 +1,14 @@
+//
+//  HomeScreenCell.swift
+//  SportsApp
+//
+//  Created by Mohamed Kotb Saied Kotb on 16/05/2024.
+//
+
+import UIKit
+
+class HomeScreenCell: UICollectionViewCell {
+    
+    @IBOutlet weak var sportName: UILabel!
+    @IBOutlet weak var sportImage: UIImageView!
+}
---
+++ SportsApp/HomeScreenCollectionViewController.swift
@@ -0,0 +1,80 @@
+//
+//  HomeScreenCollectionViewController.swift
+//  SportsApp
+//
+//  Created by Mohamed Kotb Saied Kotb on 16/05/2024.
+//
+
+import UIKit
+
+
+
+class HomeScreenCollectionViewController: UICollectionViewController,UICollectionViewDelegateFlowLayout{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        collectionView.collectionViewLayout = UICollectionViewFlowLayout()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! HomeScreenCell
+        cell.sportName.text = "Foot Ball"
+       // cell.sportImage.image = UIImage(named: "football")
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width / 2.4, height: view.frame.width / 2)
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
