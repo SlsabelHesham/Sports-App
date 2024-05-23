f22fea46c164ca727daf872b319a288fa38335b0
Slsabel Hesham <128090893+SlsabelHesham@users.noreply.github.com>
2024-05-23 22:10:03 +0300

    upcoming events and latest results

---
+++ SportsApp/Modules/LeaguesDetails/view/UpcomingEventsCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  UpcomingEventsCollectionViewCell.swift
+//  SportsApp
+//
+//  Created by Slsabel Hesham on 23/05/2024.
+//
+
+import UIKit
+
+class UpcomingEventsCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var team2Name: UILabel!
+    @IBOutlet weak var team1Name: UILabel!
+    @IBOutlet weak var team2Img: UIImageView!
+    @IBOutlet weak var team1Img: UIImageView!
+    
+}
